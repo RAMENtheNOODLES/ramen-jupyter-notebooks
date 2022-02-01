f697db9ca9f890d5b150e1062efb57d6ece87d71
RAMENtheNOODLES <58859646+RAMENtheNOODLES@users.noreply.github.com>
2022-02-01 09:37:11 -0600

    Create ai_chatbot.py

---
+++ ai_chatbot.py
@@ -0,0 +1,25 @@
+from chatterbot import ChatBot
+from chatterbot.trainers import ListTrainer
+
+mybot = ChatBot(name='Chatty', read_only=False, logic_adapters=
+               ['chatterbot.logic.BestMatch'])
+
+small_talk = [
+  'Hi there!',
+  'How do you do?',
+  'How are you?',
+  'What\'s your name?',
+  'I\'m cool',
+  'Fine, you?',
+  'I\'m okay',
+  'Glad to hear that!',
+  'Not so good',
+  'Sorry to hear that',
+  'What\'s your name?',
+  'My name is Chatty the chatbot!'
+]
+
+list_trainer = ListTrainer(mybot)
+
+for item in (small_talk):
+  list_trainer.train(item)
