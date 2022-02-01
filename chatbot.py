0d96c5b7100e431d0d1999ebf9b5f6102150a7fb
RAMENtheNOODLES <58859646+RAMENtheNOODLES@users.noreply.github.com>
2022-02-01 09:14:43 -0600

    Update chatbot.py

--- chatbot.py
+++ chatbot.py
@@ -7,7 +7,12 @@ def main():
   while True :
     print("Please ask a question and I'll answer it as best as I can!")
     response = input(">")
+    resp(response)
     
     
 def resp(response):
-  pass
+  for i in range(len(VALID_RESPONSES)):
+    for k in range(len(VALID_RESPONSES[i][0]):
+                   if response == VALID_RESPONSES[i][0][k]:
+                      print(VALID_RESPONSES[i][1]
+                      break
