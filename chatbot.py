34dc782411c2e476a4221e05af7e55bdfe0421e0
RAMENtheNOODLES <58859646+RAMENtheNOODLES@users.noreply.github.com>
2022-02-01 08:42:40 -0600

    Create chatbot.py

---
+++ chatbot.py
@@ -0,0 +1,13 @@
+# Constants
+VALID_RESPONSES = [
+  [["Hello", "Hey", "Hola"], "Hi there!"]
+]
+
+def main():
+  while True :
+    print("Please ask a question and I'll answer it as best as I can!")
+    response = input(">")
+    
+    
+def resp(response):
+  pass
