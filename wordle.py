d660dc6d15fe528677182bd14597892955abe990
RAMENtheNOODLES <58859646+RAMENtheNOODLES@users.noreply.github.com>
2022-02-24 13:03:38 -0600

    Update wordle.py

--- wordle.py
+++ wordle.py
@@ -1,4 +1,5 @@
 from colorama import init, Fore, Back, Style, Cursor
+import random
 import os
 
 max_x, max_y = os.get_terminal_size()
@@ -7,5 +8,43 @@ init(autoreset = True)
 
 pos = lambda y, x: Cursor.POS(x, y)
 
-print(Fore.GREEN + "Wordle in Python!")
-print(f"Max X: {max_x}\nMax Y: {max_y}")
+print("" + pos(1, 1))
+
+print(Fore.GREEN + "Wordle in Python! (Currently a WIP)")
+print("Please guess a 5 letter word")
+
+correct_letters = []
+guessed_letters = []
+prev_guesses = []
+word = "start"
+
+while True:
+    print("" + pos(max_y - 1, max_x - 1))
+    for i in range(len(prev_guesses)):
+        print(prev_guesses[i])
+    guess = input(">")
+    if len(guess) != 5:
+        print(Fore.RED + "Please enter a 5 letter word")
+        continue
+        
+    temp_prev_guess = ""
+    temp_guess = []
+    
+    for i in range(5):
+        print(guess[i])
+        if guess[i] == word[i]:
+            try:
+                temp_guess.remove(guess[i])
+            except:
+                pass
+            correct_letters.append(guess[i])
+            temp_prev_guess += Fore.GREEN + guess[i]
+        else:
+            for k in range(5-i):
+                if guess[i] == word[k]:
+                    temp_guess.append(guess[i])
+                    temp_prev_guess += Fore.YELLOW + str(guess[i])
+            temp_guess.append(guess[i])
+            temp_prev_guess += Fore.BLUE + str(guess[i])
+    
+    prev_guesses.append(temp_prev_guess)
