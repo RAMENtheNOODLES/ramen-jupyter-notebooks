012c54223f82c5fa3c70ff6ab1b9289b37cc801f
RAMENtheNOODLES <58859646+RAMENtheNOODLES@users.noreply.github.com>
2022-02-24 11:31:36 -0600

    Update wordle.py

--- wordle.py
+++ wordle.py
@@ -1,1 +1,11 @@
-from colorama import init, Fore, Back, Style
+from colorama import init, Fore, Back, Style, Cursor
+import os
+
+max_x, max_y = os.get_terminal_size()
+
+init(autoreset = True)
+
+pos = lambda y, x: Cursor.POS(x, y)
+
+print(Fore.GREEN + "Wordle in Python!")
+print(f"Max X: {max_x}\nMax Y: {max_y}")
