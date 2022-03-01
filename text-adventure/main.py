0e4447eefefa638afbc9b1cffaf01cfab2b730ff
RAMENtheNOODLES <58859646+RAMENtheNOODLES@users.noreply.github.com>
2022-03-01 14:17:21 -0600

    Update main.py

--- text-adventure/main.py
+++ text-adventure/main.py
@@ -3,9 +3,12 @@ from colorama import init, Fore, Back, Style, Cursor
 print("Welcome to text adventure!\nWIP")
 
 chapters = []
-c = open("story_chapters.txt")
+c = open("story_chapters.txt", "r")
 for i in c:
     print(i)
     chapters.append(i)
+    
+def read_story(chapter):
+    f = open(f"{chapter}/main.txt")
     
 
