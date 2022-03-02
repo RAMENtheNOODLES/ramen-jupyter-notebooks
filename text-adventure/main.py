771ed2e4ca9b6107a19194d20f86b35f1d2715c0
RAMENtheNOODLES <58859646+RAMENtheNOODLES@users.noreply.github.com>
2022-03-02 09:03:16 -0600

    Update main.py

--- text-adventure/main.py
+++ text-adventure/main.py
@@ -1,6 +1,24 @@
 from colorama import init, Fore, Back, Style, Cursor
+import os
+import re
+import time
 
 print("Welcome to text adventure!\nWIP")
+
+init(autoreset=True)
+
+curdir = os.getcwd()
+
+colors = {
+    "black": Fore.BLACK,
+    "red": Fore.RED,
+    "green": Fore.GREEN,
+    "yellow": Fore.YELLOW,
+    "blue": Fore.BLUE,
+    "magenta": Fore.MAGENTA,
+    "cyan": Fore.CYAN,
+    "white": Fore.WHITE
+}
 
 chapters = []
 c = open("story_chapters.txt", "r")
@@ -8,7 +26,51 @@ for i in c:
     print(i)
     chapters.append(i)
     
-def read_story(chapter):
-    f = open(f"{chapter}/main.txt")
+def read_story(chapter, option="story.txt"):
+    f = open(f"{curdir}/{chapter}/{option}", "r")
     
+    lines = f.read().split("\n")
+    
+    for i in lines:
+        # print(i)
+        
+        if i.__contains__("`"):
+            continue
+        if re.match("text, (\w*): ", i):
+            color = get_colors(i.split(", ")[1].split(":")[0])
+            # print(i.split(", ")[1].replace(":", ""))
+            print(color + i.split(": ")[1])
+            continue
+        if i.__contains__("text"):
+            print(i.split("text: ")[1])
+        if i.__contains__("prompt"):
+            choices = i.split("prompt, (")[1].split(")")[0]
+            destinations = i.split("> (")[1].split(")")[0].split(", ")
+            after_text = i.split(": ")[1]
+            # print(choices)
+            # print(destinations)
+            
+            print(f"Please select from one of these choices: ({choices})")
+            
+            while True:
+                choice = input(">")
+                
+                if choice in choices.split(", "):
+                    read_story(chapter, destinations[choices.split(", ").index(choice)])
+                    return
+                
+                print("That is not a valid option...")
+                continue
+            
+            print(after_text)
+        if i.__contains__("wait"):
+            time.sleep(int(i.split("wait, ")[1].split(":")[0]))
+            print(i.split(": ")[1])
+            
+def get_colors(color_string):
+    if color_string in colors:
+        return colors[color_string]
+    return colors["white"]
 
+read_story("beginning")
+    pass
