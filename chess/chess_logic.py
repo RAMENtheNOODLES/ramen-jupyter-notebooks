8347216a8f72f25c13f87faaeb1bfdbf8d46fbe4
RAMENtheNOODLES <58859646+RAMENtheNOODLES@users.noreply.github.com>
2022-04-01 13:24:58 -0500

    Update chess_logic.py

--- chess/chess_logic.py
+++ chess/chess_logic.py
@@ -1,5 +1,25 @@
-
+import re
 
 class logic:
-  def __init__(self):
-    pass
+    def __init__(self):
+        self.starting_pos = "rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR w KQkq - 0 1"
+    
+    def FENtoBoard(self, FEN="rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR w KQkq - 0 1"):
+        notation = self.starting_pos
+        temp = notation.split("/")
+        ranks = []
+        player = notation.split(" ")[1].split(" ")[0]
+        castleAvail = re.match(" (\w*) ", notation)
+        
+        for i in temp:
+            if i.__contains__(" "):
+                ranks.append(i.split(" ")[0])
+                break
+            ranks.append(i)
+            
+            
+
+if __name__ == "__main__":
+    l = logic()
+    
+    l.FENtoBoard()
