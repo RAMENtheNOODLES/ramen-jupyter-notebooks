a3dc674a6da7dbab7e019adcf82da6cc220d9c00
RAMENtheNOODLES <58859646+RAMENtheNOODLES@users.noreply.github.com>
2022-04-01 09:28:33 -0500

    Update main.py

--- chess/main.py
+++ chess/main.py
@@ -4,19 +4,46 @@ import os
 
 print("Chess!")
 
+init(autoreset = True)
+
 board = []
 rows = []
+
+white_pieces = {
+    "king": "♔",
+    "queen": "♕",
+    "rook": "♖",
+    "bishop": "♗",
+    "knight": "♘",
+    "pawn": "♙"
+}
+black_pieces = {
+    "king": "♚",
+    "queen": "♛",
+    "rook": "♜",
+    "bishop": "♝",
+    "knight": "♞",
+    "pawn": "♟"
+}
 
 for file in range(8):
     for rank in range(8):
         isLightSquare = (file + rank) % 2 != 0
-        print(isLightSquare)
+        # print(isLightSquare)
         
         if isLightSquare:
-            board.append(Fore.WHITE + "▀")
+            board.append(Back.GREEN + Fore.WHITE + " " + white_pieces.get("pawn") + " ")
         else:
-            board.append(Fore.BLUE + "▀")
+            board.append(Back.BLUE + Fore.BLACK + " " + white_pieces.get("pawn") + " ")
 
-row = 1
-for i in range(len(board)):
-    pass
+row = 0
+for i in range(8):
+    t = ""
+    for k in range(row*8, (row*8) + 8):
+        print(k)
+        t += board[k]
+    rows.append(t)
+    row += 1
+    
+for i in rows:
+    print(i)
