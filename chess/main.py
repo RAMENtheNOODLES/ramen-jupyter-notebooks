ff0df0fcb6261311151b6ff396485c8e1735e43b
RAMENtheNOODLES <58859646+RAMENtheNOODLES@users.noreply.github.com>
2022-04-01 08:36:43 -0500

    Update main.py

--- chess/main.py
+++ chess/main.py
@@ -1,1 +1,22 @@
+from colorama import init, Fore, Back, Style, Cursor
+import random
+import os
+
 print("Chess!")
+
+board = []
+rows = []
+
+for file in range(8):
+    for rank in range(8):
+        isLightSquare = (file + rank) % 2 != 0
+        print(isLightSquare)
+        
+        if isLightSquare:
+            board.append(Fore.WHITE + "▀")
+        else:
+            board.append(Fore.BLUE + "▀")
+
+row = 1
+for i in range(len(board)):
+    pass
