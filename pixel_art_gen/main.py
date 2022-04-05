24715ae030a17bf887b23ab3ba4436fee74b9873
RAMENtheNOODLES <58859646+RAMENtheNOODLES@users.noreply.github.com>
2022-04-05 13:14:31 -0500

    Update main.py

--- pixel_art_gen/main.py
+++ pixel_art_gen/main.py
@@ -2,3 +2,14 @@ from colorama import init, Fore, Back, Style, Cursor
 import random
 
 print("pixel art!")
+
+colors = {
+  "w": Fore.WHITE,
+  "b": Fore.BLACK,
+  "r": Fore.RED,
+  "y": Fore.YELLOW,
+  "g": Fore.GREEN,
+  "m": Fore.MAGENTA,
+  "u": Fore.BLUE,
+  "c": Fore.CYAN
+}
