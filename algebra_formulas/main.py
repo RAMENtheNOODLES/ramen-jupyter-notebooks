db7946802804d25e848cc3e2c9ea7072fa47e37f
RAMENtheNOODLES <58859646+RAMENtheNOODLES@users.noreply.github.com>
2022-05-02 09:48:08 -0500

    Update main.py

--- algebra_formulas/main.py
+++ algebra_formulas/main.py
@@ -1,1 +1,28 @@
+import math
+from fractions import Fraction
 
+class AlgebraFormulas:
+    def __init__(self):
+        pass
+    
+    @staticmethod
+    def Quadratic(a, b, c, solve=False):
+        x = [0, 0]
+        
+        x[0] = (-b + math.sqrt(b**2 - 4*a*c)) / (2*a)
+        x[1] = (-b - math.sqrt(b**2 - 4*a*c)) / (2*a)
+        
+        if not solve:
+            return x
+        
+    
+    @staticmethod
+    def DTF(decimal):
+        return Fraction(decimal)
+    
+
+if __name__ == "__main__":
+    AF = AlgebraFormulas
+    print(AF.Quadratic(1, 6, -27))
+    
+    print(AF.DTF(1))
