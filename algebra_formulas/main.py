df37274a6fb6bea71205daff5646deb6f4358944
RAMENtheNOODLES <58859646+RAMENtheNOODLES@users.noreply.github.com>
2022-05-02 10:20:04 -0500

    Update main.py

--- algebra_formulas/main.py
+++ algebra_formulas/main.py
@@ -7,6 +7,7 @@ class AlgebraFormulas:
     
     @staticmethod
     def Quadratic(a, b, c, solve=False):
+        AF = AlgebraFormulas
         x = [0, 0]
         
         x[0] = (-b + math.sqrt(b**2 - 4*a*c)) / (2*a)
@@ -15,14 +16,19 @@ class AlgebraFormulas:
         if not solve:
             return x
         
+        paren1 = f"({AF.DTF(x[0])})"
+        
     
     @staticmethod
-    def DTF(decimal):
+    def DTF(decimal, returnFraction=False):
+        if returnFraction:
+            f = Fraction(decimal)
+            return f if (f * (1/(decimal)) == 1) else f"{f}/1"
         return Fraction(decimal)
     
 
 if __name__ == "__main__":
     AF = AlgebraFormulas
     print(AF.Quadratic(1, 6, -27))
-    
-    print(AF.DTF(1))
+    print(1/(1/2))
+    print(AF.DTF(1, True))
